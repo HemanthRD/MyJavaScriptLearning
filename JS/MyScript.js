01a1065616499fd6311752b77b28fdda211daef9
HEMANTHA R.D <62497063+HemanthRD@users.noreply.github.com>
2023-03-20 18:42:40 +0530

    Add files via upload
    Initial commit

---
+++ JS/MyScript.js
@@ -0,0 +1,28 @@
+alert("Hello world!");
+alert('Hai')
+alert('I am RaviChandran')
+
+/* A semicolon may be omitted in most cases when a line break exists. 
+  Here, JavaScript interprets the line break as an “implicit” semicolon. This
+  is called an automatic semicolon insertion.
+  In most cases, a newline implies a semicolon. But “in most cases” does not
+  mean “always”! For example:
+
+  alert(3 +
+   1
+   +2);
+
+   The code outputs 6 because JavaScript does not insert semicolons here. It is 
+   intuitively obvious that if the line ends with a plus "+", then it is an 
+   “incomplete expression”, so a semicolon there would be incorrect. And in 
+   this case, that works as intended.
+
+   But there are situations where JavaScript “fails” to assume a semicolon 
+   where it is really needed.
+
+   We recommend putting semicolons between statements even if they are separated
+    by newlines. This rule is widely adopted by the community. 
+
+*/
+
+// one line comment
---
+++ JS/Variables.js
@@ -0,0 +1,20 @@
+let name="Hemanth", age=41, email="hemuudupi@gmail.com";
+let contactNo=9980212691;
+let sex="male";
+console.log(name);
+console.log(age);
+console.log(email);
+console.log(contactNo);
+console.log(sex);
+
+
+let hello = 'Hello world!';
+
+let message;
+
+// copy 'Hello world' from hello into message
+message = hello;
+
+// now two variables hold the same data
+alert(hello); // Hello world!
+alert(message); // Hello world!
